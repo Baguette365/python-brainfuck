67cf3d3c9ced4993d604b060302ed145d02a949a
Baguette365 <162833778+Baguette365@users.noreply.github.com>
2024-07-04 18:46:53 +0200

    add the english version

---
+++ brainfuck english command-line.py
@@ -0,0 +1,38 @@
+#compilateur de brainfuck écrit en python//brainfuck compilator writen in python
+
+braincode=input('(brainfuck code ) >>> ')
+position=0
+memory=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+def compile():
+    global braincode
+    global position
+    global memory
+    print(memory)
+    for i in range(len(braincode)):
+        if braincode[i]==',':
+            memory[position]=ord(i+1)
+        else:
+            if braincode[i]=='+':
+                memory[position]+=1
+            if braincode[i]=='-':
+                memory[position]-=1
+            if braincode[i]=='<':
+                if memory[position]<0:
+                    position-=1
+            if braincode[i]=='>':
+                if memory[position]>0:
+                    position+=1
+            if braincode[i]=='.':
+                memory[position]=chr(memory[position])
+
+            if braincode[i]=='[':
+                if memory[position]==0:
+                    position=memory[position]
+            if braincode[i]==']':
+                if memory[position]!=0:
+                    position-=1
+        print(memory)
+
+compile()
+print("compilation end")
+print("restart the script for another compilation !!! ")
